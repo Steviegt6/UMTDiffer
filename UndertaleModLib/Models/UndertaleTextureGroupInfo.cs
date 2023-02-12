39ab51aa3ee769182ba8f284c739652750749aca
Tomat <xxlennygamerxx@gmail.com>
2023-02-12 04:44:52 -0800

    Completely hide Spine Sprites, skip them during (de)serialization.

--- UndertaleModLib/Models/UndertaleTextureGroupInfo.cs
+++ UndertaleModLib/Models/UndertaleTextureGroupInfo.cs
@@ -56,10 +56,10 @@ public class UndertaleTextureGroupInfo : UndertaleNamedResource, IDisposable
     /// </summary>
     public UndertaleSimpleResourcesList<UndertaleSprite, UndertaleChunkSPRT> Sprites { get; set; }
 
-    /// <summary>
-    /// A list of spine sprites the group has.
-    /// </summary>
-    public UndertaleSimpleResourcesList<UndertaleSprite, UndertaleChunkSPRT> SpineSprites { get; set; }
+    // /// <summary>
+    // /// A list of spine sprites the group has.
+    // /// </summary>
+    // public UndertaleSimpleResourcesList<UndertaleSprite, UndertaleChunkSPRT> SpineSprites { get; set; }
 
     /// <summary>
     /// A list of fonts the group has.
@@ -114,7 +114,7 @@ public class UndertaleTextureGroupInfo : UndertaleNamedResource, IDisposable
     {
         TexturePages = new UndertaleSimpleResourcesList<UndertaleEmbeddedTexture, UndertaleChunkTXTR>();
         Sprites = new UndertaleSimpleResourcesList<UndertaleSprite, UndertaleChunkSPRT>();
-        SpineSprites = new UndertaleSimpleResourcesList<UndertaleSprite, UndertaleChunkSPRT>();
+        // SpineSprites = new UndertaleSimpleResourcesList<UndertaleSprite, UndertaleChunkSPRT>();
         Fonts = new UndertaleSimpleResourcesList<UndertaleFont, UndertaleChunkFONT>();
         Tilesets = new UndertaleSimpleResourcesList<UndertaleBackground, UndertaleChunkBGND>();
     }
@@ -133,13 +133,13 @@ public class UndertaleTextureGroupInfo : UndertaleNamedResource, IDisposable
 
         writer.WriteUndertaleObjectPointer(TexturePages);
         writer.WriteUndertaleObjectPointer(Sprites);
-        writer.WriteUndertaleObjectPointer(SpineSprites);
+        // writer.WriteUndertaleObjectPointer(SpineSprites);
         writer.WriteUndertaleObjectPointer(Fonts);
         writer.WriteUndertaleObjectPointer(Tilesets);
 
         writer.WriteUndertaleObject(TexturePages);
         writer.WriteUndertaleObject(Sprites);
-        writer.WriteUndertaleObject(SpineSprites);
+        // writer.WriteUndertaleObject(SpineSprites);
         writer.WriteUndertaleObject(Fonts);
         writer.WriteUndertaleObject(Tilesets);
     }
@@ -159,14 +159,14 @@ public class UndertaleTextureGroupInfo : UndertaleNamedResource, IDisposable
         // Read the pointers
         TexturePages = reader.ReadUndertaleObjectPointer<UndertaleSimpleResourcesList<UndertaleEmbeddedTexture, UndertaleChunkTXTR>>();
         Sprites = reader.ReadUndertaleObjectPointer<UndertaleSimpleResourcesList<UndertaleSprite, UndertaleChunkSPRT>>();
-        SpineSprites = reader.ReadUndertaleObjectPointer<UndertaleSimpleResourcesList<UndertaleSprite, UndertaleChunkSPRT>>();
+        // SpineSprites = reader.ReadUndertaleObjectPointer<UndertaleSimpleResourcesList<UndertaleSprite, UndertaleChunkSPRT>>();
         Fonts = reader.ReadUndertaleObjectPointer<UndertaleSimpleResourcesList<UndertaleFont, UndertaleChunkFONT>>();
         Tilesets = reader.ReadUndertaleObjectPointer<UndertaleSimpleResourcesList<UndertaleBackground, UndertaleChunkBGND>>();
 
         // Read the objects, throwing an error if the pointers are invalid
         reader.ReadUndertaleObject(TexturePages);
         reader.ReadUndertaleObject(Sprites);
-        reader.ReadUndertaleObject(SpineSprites);
+        // reader.ReadUndertaleObject(SpineSprites);
         reader.ReadUndertaleObject(Fonts);
         reader.ReadUndertaleObject(Tilesets);
     }
@@ -185,7 +185,7 @@ public class UndertaleTextureGroupInfo : UndertaleNamedResource, IDisposable
         Name = null;
         TexturePages = null;
         Sprites = null;
-        SpineSprites = null;
+        // SpineSprites = null;
         Fonts = null;
         Tilesets = null;
     }
